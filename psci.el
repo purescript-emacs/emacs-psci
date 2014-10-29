43cf04ecab317a7a39c882c5b249ed721f06ea4f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-29 20:06:35 +0000

    - [X] Update heading information about the minor-mode activation and setup

--- psci.el
+++ psci.el
@@ -28,13 +28,21 @@
 
 ;;; Commentary:
 
-;; Provides a nice interface to evaluating Purescript expression.
-;; Input is handled by the comint package, and output is passed
-;; through the pretty-printer.
+;; Provides a simple interface to evaluate Purescript expression.
+;; Input is handled by the comint package.
 
-;; To start: M-x psci.  Type C-h m in the *psci* buffer for more info.
+;; To start psci repl:
+;; M-x psci.  Type C-h m in the *psci* buffer for more info.
 
-;;
+;; To activate some basic bindings, you can add the following hook
+;; to purescript-mode:
+;; (add-hook 'purescript-mode-hook 'inferior-psci-mode)
+
+;; To activate psci directly from a purescript-mode buffer, you
+;; could use repl-toggle (available on melpa):
+;; (require 'repl-toggle)
+;; (add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
+
 ;; More informations: https://ardumont/emacs-psci
 ;; Issue tracker: https://github.com/ardumont/emacs-psci/issues
 
