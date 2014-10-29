c8e984beec0cb95616961d44a4b5b47b84efd13c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-29 12:19:20 +0100

    Add some helper to ensure the mode's setup is as expected.

--- psci.el
+++ psci.el
@@ -127,6 +127,35 @@ See `'comint-send-input`' for more information."
   "Run purescript code between REGION-START and REGION-END."
   (let ((process (get-buffer-process (psci/process-name psci/buffer-name))))
     (comint-send-region process region-start region-end)
+;; ELISP> (with-current-buffer "*psci*"
+;;          (psci/--debug-setup!))
+;; ("comint-prompt-regexp" "^> "
+;;  "paragraph-separate" "\\'"
+;;  "paragraph-start" "^> "
+;;  "comint-input-sender" comint-simple-send
+;;  "comint-get-old-input" comint-get-old-input-default
+;;  "comint-process-echoes" nil
+;;  "comint-prompt-read-only" t
+;;  "comint-eol-on-send" t
+;;  "comint-input-filter-functions" nil
+;;  "font-lock-defaults" (purescript-font-lock-keywords t)
+;;  "comment-start" "-- "
+;;  "comment-use-syntax" t)
+
+(defun psci/--debug-setup! ()
+  "A function to debug the current major mode's runtime setup."
+  (list "comint-prompt-regexp" comint-prompt-regexp
+        "paragraph-separate" paragraph-separate
+        "paragraph-start" paragraph-start
+        "comint-input-sender" comint-input-sender
+        "comint-get-old-input" comint-get-old-input
+        "comint-process-echoes" comint-process-echoes
+        "comint-prompt-read-only" comint-prompt-read-only
+        "comint-eol-on-send" comint-eol-on-send
+        "comint-input-filter-functions" comint-input-filter-functions
+        "font-lock-defaults" font-lock-defaults
+        "comment-start" comment-start
+        "comment-use-syntax" comment-use-syntax))
     (process-send-eof process)))
 
 (defun psci/--load-file! (filename)
