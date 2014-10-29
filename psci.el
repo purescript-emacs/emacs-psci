aaad6fe0e0f87589a10ef7a83ef87db4ea205d20
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-29 16:23:52 +0100

    Keep psci's default behavior - need to manually send C-d to validate the input.

--- psci.el
+++ psci.el
@@ -62,20 +62,9 @@
 
 (defvar psci-mode-map
   (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
-    ;; example definition
-    (define-key map "\t"     'completion-at-point)
-    (define-key map (kbd "C-m") 'psci/send-input)
-    (define-key map (kbd "C-j") 'psci/send-input)
+    (define-key map "\t" 'completion-at-point)
     map)
   "Basic mode map for `psci'.")
-
-(defun psci/send-input (&optional no-newline artificial)
-  "Send input and force the eof signal.
-NO-NEWLINE and ARTIFICIAL are optional.
-See `'comint-send-input`' for more information."
-  (interactive)
-  (comint-send-input no-newline artificial)
-  (comint-send-eof))
 
 (defvar psci/prompt "> "
   "The psci prompt.")
