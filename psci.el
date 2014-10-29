2d182c594b512c7e61384efedf3d86f87e58dd3c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-29 16:10:12 +0100

    Extract dev utils inside their own namespace

--- psci.el
+++ psci.el
@@ -117,36 +117,6 @@ See `'comint-send-input`' for more information."
   (setq-local comment-start "-- ")
   (setq-local comment-use-syntax t))
 
-;; ELISP> (with-current-buffer "*psci*"
-;;          (psci/--debug-setup!))
-;; ("comint-prompt-regexp" "^> "
-;;  "paragraph-separate" "\\'"
-;;  "paragraph-start" "^> "
-;;  "comint-input-sender" comint-simple-send
-;;  "comint-get-old-input" comint-get-old-input-default
-;;  "comint-process-echoes" nil
-;;  "comint-prompt-read-only" t
-;;  "comint-eol-on-send" t
-;;  "comint-input-filter-functions" nil
-;;  "font-lock-defaults" (purescript-font-lock-keywords t)
-;;  "comment-start" "-- "
-;;  "comment-use-syntax" t)
-
-(defun psci/--debug-setup! ()
-  "A function to debug the current major mode's runtime setup."
-  (list "comint-prompt-regexp" comint-prompt-regexp
-        "paragraph-separate" paragraph-separate
-        "paragraph-start" paragraph-start
-        "comint-input-sender" comint-input-sender
-        "comint-get-old-input" comint-get-old-input
-        "comint-process-echoes" comint-process-echoes
-        "comint-prompt-read-only" comint-prompt-read-only
-        "comint-eol-on-send" comint-eol-on-send
-        "comint-input-filter-functions" comint-input-filter-functions
-        "font-lock-defaults" font-lock-defaults
-        "comment-start" comment-start
-        "comment-use-syntax" comment-use-syntax))
-
 (defun psci/--run-psci-command! (command)
   "Run psci COMMAND as string."
   (-when-let (process (get-buffer-process (psci/process-name psci/buffer-name)))
